8aaa28748a20e1e7cf092bcdf3fd505dbecc7928
Robert L Carpenter <robert@robacarp.com>
2018-11-25 13:52:58 -0700

    update to allow use of time.utc_now, crystal 0.27

--- src/timecop/core_ext/time.cr
+++ src/timecop/core_ext/time.cr
@@ -3,15 +3,15 @@
 struct Time
   @@mock : Bool = true
 
-  def self.now_without_mock_time
+  def self.now_without_mock_time(location : Location = Location.local)
     @@mock = false
-    result = now
+    result = now(location)
     @@mock = true
     result
   end
 
-  def self.now
+  def self.now(location : Location = Location.local) : Time
     return previous_def if !Timecop.frozen? || !@@mock
-    Timecop.top_stack_item.time
+    Timecop.top_stack_item.time(location)
   end
 end
